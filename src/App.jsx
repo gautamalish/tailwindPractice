925d7d22fe916d4dbeac573566b39c4798a6117f
zyzzchad <119152524+gautamalish@users.noreply.github.com>
2024-08-11 06:25:56 +0545

    Create all the required components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
 import { useState } from "react";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import PopularProducts from "./components/PopularProducts";
+import SuperQuality from "./components/SuperQuality";
+import Services from "./components/Services";
+import SpecialOffers from "./components/SpecialOffers";
+import CustomerReviews from "./components/CustomerReviews";
+import Subscribe from "./components/Subscribe";
+import Footer from "./components/Footer";
 function App() {
-  return <></>;
+  return (
+    <main className="relative">
+      <Nav />
+      <section className="xl:padding-1 wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="bg-pale-blue padding">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding-x bg-black padding-t pb-8">
+        <Footer />
+      </section>
+    </main>
+  );
 }
 
 export default App;
